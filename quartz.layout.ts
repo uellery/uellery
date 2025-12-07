006514efe912c33058c1778510147f1d3b7d9e36
Yunus Emre Yalçın <emrellery@gmail.com>
2025-12-07 11:47:53 +0300

    .

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Instagram: "https://instagram.com/emrellery/",
+      "Twitter": "https://x.com/nicinbilmiyorum",
     },
   }),
 }
